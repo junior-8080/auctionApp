f8bd86454f8c065e1dea92800492f1d75b4ae909
Jonathan Bonzali <katbonzali@gmail.com>
2023-09-25 09:35:37 +0000

    clean up

--- src/utils/form.ts
+++
@@ -1,7 +0,0 @@
-//returns an objects containing values whose key are in the keys param ; replaces undefined and null value for each key with an empty string
-export const formInit = (item: any, keys: string[]) => {
-  return keys.reduce((subSet: any, key) => {
-    subSet[key] = item[key] || "";
-    return subSet;
-  }, {});
-};
