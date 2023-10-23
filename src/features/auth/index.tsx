4f2fe94e18dd6e1a5c910866ac7719f18fac29fd
SahTitus <sahtitus58@gmail.com>
2023-10-23 19:01:40 +0000

    landing page - navbar, product listing and footer

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = (page : any) => {
   return (
     <div className="w-full h-screen flex flex-row items-stretch ">
-     <h1>Hello world</h1>
+     <h1>Hello </h1>
     </div>
   );
 };
