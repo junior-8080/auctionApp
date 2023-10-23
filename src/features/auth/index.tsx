005a7171b9bdea711b2520aa0615226fb29782bf
SahTitus <sahtitus58@gmail.com>
2023-10-23 19:06:00 +0000

    git commit -m "landing page - navbar, product listing and footer"

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = (page : any) => {
   return (
     <div className="w-full h-screen flex flex-row items-stretch ">
-     <h1>Hello </h1>
+     <h1>Hel</h1>
     </div>
   );
 };
