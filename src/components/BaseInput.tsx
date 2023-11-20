096d224504c7fedb6b8176115f2024185ea9531f
Jonathan Bonzali <katbonzali@gmail.com>
2023-11-20 21:07:24 +0000

    save state

--- src/components/BaseInput.tsx
+++ src/components/BaseInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef, Ref } from "react";
 
 type Props = {
   label: string;
@@ -9,34 +9,34 @@ type Props = {
   name?: string;
 };
 
-function BaseInput({
-  label,
-  name,
-  id,
-  placeholder,
-  type,
-  errorMessage,
-}: Props) {
-  return (
-    <div className={"mt-5"}>
-      <div className="relative">
-        <input
-          name={name}
-          className="block px-2.5 py-5 text-md rounded border border-gray-300 w-full text-gray-900 bg-transparent appearance-none focus:outline-2 focus:outline-primary-500 focus:ring-0 focus:border-primary-500 peer"
-          id={id}
-          placeholder={placeholder}
-          type={type}
-        />
-        <label
-          className=" w-[80%] absolute text-md text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1/3 peer-focus:scale-75 peer-focus:-translate-y-5 left-1"
-          htmlFor={id}
-        >
-          {label}
-        </label>
+const BaseInput = forwardRef(
+  (
+    { label, id, placeholder, type, errorMessage, ...rest }: Props,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    return (
+      <div className={"mt-5"}>
+        <div className="relative">
+          <input
+            autoComplete={"off"}
+            {...rest}
+            ref={ref}
+            className="autofill:bg-transparent block px-2.5 py-5 text-md rounded-md border border-gray-300 w-full text-gray-900 bg-transparent appearance-none focus:outline-2 focus:outline-primary-500 focus:ring-0 focus:border-primary-500 peer"
+            id={id}
+            placeholder={placeholder}
+            type={type}
+          />
+          <label
+            className=" w-[80%] absolute text-md text-gray-500 duration-300 transform -translate-y-4 scale-75 top-1/3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1/3 peer-focus:scale-75 peer-focus:-translate-y-5 left-1"
+            htmlFor={id}
+          >
+            {label}
+          </label>
+        </div>
+        <p className={"text-xs text-red-400"}>{errorMessage}</p>
       </div>
-      <p className={"text-xs text-red-400"}>{errorMessage}</p>
-    </div>
-  );
-}
+    );
+  },
+);
 
 export default BaseInput;
--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,25 +1,43 @@
+"use client";
 import React from "react";
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler } from "react-hook-form";
 import BaseInput from "@components/BaseInput"; // Importing BaseInput from the library
 import { FormInputs } from "./lib/FormInputType";
+import * as Yup from "yup";
+import { useYupResolver } from "@hooks/useYupResolver";
+
+export const loginValidationSchema = Yup.object().shape({
+  email: Yup.string().required("This field is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long.")
+    .max(128, "Password must be at most 128 characters long.")
+    .required("Password is required."),
+});
 
 function LoginForm() {
-  const { register, handleSubmit } = useForm<FormInputs>();
+  const resolver = useYupResolver(loginValidationSchema);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({ resolver, mode: "onChange" });
   const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={"px-10 py-10"}>
       <div className="flex flex-col gap-y-5">
         <BaseInput
           type="email"
-          {...register('email')}
+          {...register("email")}
+          errorMessage={errors.email ? errors.email.message : ""}
           id={"email"}
           placeholder={"Enter email"}
-          label={"Email"}        
+          label={"Email"}
         />
         <BaseInput
           type="password"
-          {...register('password')}
+          errorMessage={errors.password ? errors.password.message : ""}
+          {...register("password")}
           id={"password"}
           placeholder={"Enter Password"}
           label={"password"}
@@ -27,6 +45,7 @@ function LoginForm() {
       </div>
 
       <button
+        type={"submit"}
         className={`mt-5 w-full bg-teal rounded-lg justify-center font-bold h-[54px]`}
       >
         Sign In
